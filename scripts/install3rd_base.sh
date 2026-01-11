24e599fe6c629cb72cc033c3317417e20370dc6f
Sway <zhswee@gmail.com>
2026-01-11 22:53:49 +0800

    Update install3rd_base.sh

--- scripts/install3rd_base.sh
+++ scripts/install3rd_base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # system level dependencies
-sudo apt-get install -y build-essential cmake zlib1g-dev freeglut3-dev libbz2-dev libglib2.0 python3-distutils python3-dev python3-pip python3-tk
+sudo apt-get install -y build-essential cmake zlib1g-dev freeglut3-dev libbz2-dev libglib2.0 python3-dev python3-pip python3-tk
 
 # Qt5 based library
 #if [ $(lsb_release -rs) = "20.04" ]; then # replace 8.04 by the number of release you want
